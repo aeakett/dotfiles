211472a44b2a6ade4808dc5259a2b0b171426caf
Andrew Eakett <aeakett@ssm-webprod.nrcan.gc.ca>
2013-01-29 15:18:59 -0500

    add .dmg mounting to extract() (thanks ttscoff)

--- .oh-my-zsh/custom/functions.zsh
+++ .oh-my-zsh/custom/functions.zsh
@@ -3,11 +3,11 @@
 extract () {
    if [ -f $1 ] ; then
       case $1 in
-         *.tar.bz2)   tar xjf $1     ;;
-         *.tar.gz)    tar xzf $1     ;;
-         *.tar)       tar xf $1      ;;
-         *.tgz)       tar xzf $1     ;;
-         *.tbz2)      tar xjf $1     ;;
+         *.tar.bz2)   tar xvjf $1     ;;
+         *.tar.gz)    tar xvzf $1     ;;
+         *.tar)       tar xvf $1      ;;
+         *.tgz)       tar xvzf $1     ;;
+         *.tbz2)      tar xvjf $1     ;;
          *.gz)        gunzip $1      ;;
          *.bz2)       bunzip2 $1     ;;
          *.rar)       unrar e $1     ;;
@@ -19,6 +19,7 @@ extract () {
          *.cb7)       mkdir $1-extracted; cd $1-extracted; cp ../$1 ./; 7z x $1; rm $1; cd ..        ;;
          *.jar)       mkdir $1-extracted; cd $1-extracted; cp ../$1 ./; unzip $1; rm $1; cd ..       ;;
          *.epub)      mkdir $1-extracted; cd $1-extracted; cp ../$1 ./; unzip $1; rm $1; cd ..       ;;
+			*.dmg)       hdiutil mount $1 ;;
          *)           echo "'$1' cannot be extracted via extract()" ;;
       esac
    else
