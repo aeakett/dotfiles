f5d0077a4855a2ba2519ff11e3ddb96e99ec55b0
aeakett <andrew.eakett@canada.ca>
2026-08-07 09:11:21 -0400

    add local man path

--- bash/.bashrc.d/00-env.sh
+++ bash/.bashrc.d/00-env.sh
@@ -1,6 +1,7 @@
 # Path, exports, and environment variables
 
 export PATH=$PATH:~/bin/
+export MANPATH="$HOME/.local/share/man:$MANPATH"
 
 # Start ssh-agent if not running
 if [ -z "$SSH_AUTH_SOCK" ]; then
