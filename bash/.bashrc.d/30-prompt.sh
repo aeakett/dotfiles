c259475e039f8c8ef2bc288e88782db02f77769f
aeakett <aeakett@gmail.com>
2026-08-13 10:08:55 -0400

    point to eza theme if there is one

--- bash/.bashrc.d/30-prompt.sh
+++ bash/.bashrc.d/30-prompt.sh
@@ -1,2 +1,4 @@
 # PS1, colors, and terminal prompt setup
 # since we're using powerline, this will be pretty much empty
+
+[[ -f "$HOME/.bashrc.d/theme-eza.inc" ]] && source "$HOME/.bashrc.d/theme-eza.inc"
