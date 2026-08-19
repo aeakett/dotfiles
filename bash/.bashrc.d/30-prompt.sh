4d5d5c1b631faf823e6ddd53dbaa502a1e22a644
aeakett <aeakett@gmail.com>
2026-08-19 19:22:03 -0400

    load .dir_colors

--- bash/.bashrc.d/30-prompt.sh
+++ bash/.bashrc.d/30-prompt.sh
@@ -1,6 +1,9 @@
 # PS1, colors, and terminal prompt setup
 # Also include any theme stuff... this is where we make stuff pretty
 
+# load custom .dir_colors
+eval "$(dircolors ~/.dir_colors)
+
 [[ -f "$HOME/.bashrc.d/theme-eza.inc" ]] && source "$HOME/.bashrc.d/theme-eza.inc"
 [[ -f "$HOME/.bashrc.d/theme-fzf.inc" ]] && source "$HOME/.bashrc.d/theme-fzf.inc"
 [[ -f "$HOME/.bashrc.d/theme-bat.inc" ]] && source "$HOME/.bashrc.d/theme-bat.inc"
