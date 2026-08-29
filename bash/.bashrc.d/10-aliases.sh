2757fe81c2099acf4bb6e12f79fbf8fce92ab9e2
aeakett <aeakett@gmail.com>
2026-08-28 20:06:13 -0400

    add alias to vanilla "tree"

--- bash/.bashrc.d/10-aliases.sh
+++ bash/.bashrc.d/10-aliases.sh
@@ -37,6 +37,7 @@ alias lsda='ls -la|grep "^d"'
 if command -v eza >/dev/null 2>&1; then
    alias ls='eza -F --group-directories-first --icons=auto --git --color-scale=all --time-style=long-iso'
    alias tree='eza --tree --icons=auto --color=auto'
+	alias otree='command tree'
    alias lsd='ls -D'
    alias lsda='ls -Da'
 fi
